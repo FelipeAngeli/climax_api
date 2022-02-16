cbb02059898f37892b732acc0aec137f98001d45
felipeangeli <felipe.angeli@gmail.com>
2022-02-16 12:42:31 -0300

    tela

--- lib/app/view/home_page.dart
+++ lib/app/view/home_page.dart
@@ -2,6 +2,7 @@ import 'package:climax/app/controller/home_controller.dart';
 import 'package:climax/app/services/dio_client.dart';
 import 'package:climax/app/services/json_placeholder_service.dart';
 import 'package:flutter/material.dart';
+//https://www.youtube.com/watch?v=y_HpEEYatro
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,37 +20,111 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: const Text("Clima"),
         ),
-        body: AnimatedBuilder(
-          // ouve o notifyListeners
-          animation: controller,
-          builder: (context, widget) {
-            return Column(
-              children: [
-                TextFormField(
-                  controller: controller.cidadeController,
-                  onFieldSubmitted: (value) {
-                    controller.fetchAllTodos();
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: TextFormField(
+                    controller: controller.cidadeController,
+                    decoration: InputDecoration(
+                        labelText: "Digite sua cidade",
+                        labelStyle: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        alignLabelWithHint: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(width: 3, color: Colors.blue),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              width: 3, color: Colors.blueAccent),
+                          borderRadius: BorderRadius.circular(15),
+                        ))),
+              ),
+              SizedBox(
+                width: 250,
+                height: 55,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    await controller.fetchAllTodos();
                   },
+                  child: const Text(
+                    "Consultar",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15))),
                 ),
-                SizedBox(
-                  height: 400,
-                  child: ListView.builder(
-                      itemCount: controller.todos.length,
-                      itemBuilder: (context, index) {
-                        final todo = controller.todos[index];
-                        return ListTile(
-                            title: Column(
-                          children: [
-                            Text(todo.day),
-                            Text(todo.temperature),
-                            Text(todo.wind),
-                          ],
-                        ));
-                      }),
-                ),
-              ],
-            );
-          },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                height: 400,
+                child: AnimatedBuilder(
+                    animation: controller,
+                    builder: (context, _) {
+                      return ListView.builder(
+                          itemCount: controller.todos.length,
+                          itemBuilder: (context, index) {
+                            final todo = controller.todos[index];
+
+                            return ListTile(
+                                title: Column(
+                              children: [
+                                const SizedBox(height: 10),
+                                Text("Dia " + todo.day),
+                                Text("Temperatura " + todo.temperature),
+                                Text("Vento " + todo.wind),
+                              ],
+                            ));
+                          });
+                    }),
+              ),
+            ],
+          ),
         ));
   }
 }
+
+
+
+
+
+// AnimatedBuilder(
+//           // ouve o notifyListeners
+//           animation: controller,
+//           builder: (context, widget) {
+//             return Column(
+//               children: [
+//                 TextFormField(
+//                   controller: controller.cidadeController,
+//                   onFieldSubmitted: (value) {
+//                     controller.fetchAllTodos();
+//                   },
+//                 ),
+//                 SizedBox(
+//                   height: 400,
+//                   child: ListView.builder(
+//                       itemCount: controller.todos.length,
+//                       itemBuilder: (context, index) {
+//                         final todo = controller.todos[index];
+//                         return ListTile(
+//                             title: Column(
+//                           children: [
+//                             Text(todo.day),
+//                             Text(todo.temperature),
+//                             Text(todo.wind),
+//                           ],
+//                         ));
+//                       }),
+//                 ),
+//               ],
+//             );
+//           },
+//         )
